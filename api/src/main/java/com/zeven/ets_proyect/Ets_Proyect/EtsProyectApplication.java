147ff7426aee0f5d33073574d31f287ad3742263
JonayCastro <zeven8540@gmail.com>
2025-04-21 19:05:09 +0100

    Created backend

---
+++ api/src/main/java/com/zeven/ets_proyect/Ets_Proyect/EtsProyectApplication.java
@@ -0,0 +1,13 @@
+package com.zeven.ets_proyect.Ets_Proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtsProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtsProyectApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/zeven/ets_proyect/Ets_Proyect/EtsProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeven.ets_proyect.Ets_Proyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtsProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
