76ac87c3702254b5ad0db1e968e0be825041fa09
JonayCastro <zeven8540@gmail.com>
2025-05-25 13:17:28 +0100

    Added process to delete favorites

--- app/src/app/config/paths.ts
+++ app/src/app/config/paths.ts
@@ -5,10 +5,10 @@ export default class Paths {
     public static API_BASE_URL: string = "http://localhost";
     public static API_PORT: number = 8888;
     public static CREATE_PATH: string = "create";
-    public static DELETE_PATH: string = "delete";
     public static FAVORITE_PATH: string = "favorites";
     public static LIST_PATH: string = "list";
     public static LOGIN_PATH: string = "login";
+    public static REMOVE_PATH: string = "remove";
     public static SNEAKERS_PATH: string = "sneakers";
     public static STORED_PRODUCTS_PATH: string = "stored-products"
     public static SUPPLIER_DATA_PATH: string = "supplier-data";
